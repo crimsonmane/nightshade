20a1b982910204c12e1f4f78b1a64bba67fda521
crimsonmane <crimsonmane@gmail.com>
2013-12-13 14:38:34 -0600

    Update medibuntu.sh

--- medibuntu.sh
+++ medibuntu.sh
@@ -1,5 +1,3 @@
-cd /etc/apt
-sudo rm sources.list.d/medibuntu*
-sudo cp sources.list sources.list.bkup
-sudo sed -i '/medibuntu/d' sources.list
-cd ~
+sudo rm /etc/apt/sources.list.d/medibuntu*
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bkup
+sudo sed -i '/medibuntu/d' /etc/apt/sources.list
